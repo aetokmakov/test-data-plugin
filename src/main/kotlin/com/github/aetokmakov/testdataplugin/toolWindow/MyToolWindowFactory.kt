f6c72ca8cf8563a55101c9d1c56460437e0ab29a
GitHub Action <action@github.com>
2023-08-11 15:49:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/aetokmakov/testdataplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/aetokmakov/testdataplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.aetokmakov.testdataplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.aetokmakov.testdataplugin.MyBundle
+import com.github.aetokmakov.testdataplugin.services.MyProjectService
 import javax.swing.JButton
 
 
